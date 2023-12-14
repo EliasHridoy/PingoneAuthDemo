25d1062c8b1c7a86256b0ae5bb1114e43de6d0e5
Elias Hridoy <eliashridoy.cse@gmail.com>
2023-12-15 01:11:09 +0600

    registration services added

---
+++ Models/RegistrationViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace PingOneDemo.Model
+{    
+    public class Name
+    {
+        public string given { get; set; }
+        public string family { get; set; }
+    }
+
+    public class Population
+    {
+        public string id { get; set; }
+    }
+    public class Password
+    {
+        public string value { get; set; }
+        public bool forceChange { get; set; }
+    }
+    public class RegistrationModel
+    {
+        public string email { get; set; }
+        public Name name { get; set; }
+        public Population population { get; set; }
+        public string username { get; set; }
+        public string department { get; set; }
+        public List<string> locales { get; set; }
+        public Password password { get; set; }
+    }
+
+    public class RegistrationViewModel
+    {
+        public string email { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+    }
+}
--- Services/PingoneAuthServies.cs
+++ Services/PingoneAuthServies.cs
@@ -1,16 +1,15 @@
-﻿using System;
+﻿using PingOneDemo.Model;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
-using System.Configuration;
 
 namespace PingoneAuthDemo.Services
 {
-   public class PingoneAuthServices
+    public class PingoneAuthServices
     {
         public async Task<string> Login()
         {
@@ -22,6 +21,70 @@ namespace PingoneAuthDemo.Services
 
                 string url = $"{authPath}/{envId}/as/authorize?response_type=token&client_id={clientId}&scope=openid&redirect_uri=http://localhost:8080/PingOne/index";
                 return url;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<string> GetAccessToken()
+        {
+            //var client = _httpClientFactory.CreateClient("PingOneApiClient");
+
+            var authPath = ConfigurationManager.AppSettings["PingOne:authPath"];
+            var clientId = ConfigurationManager.AppSettings["PingOne:adminAppID"];
+            var clientSecret = ConfigurationManager.AppSettings["PingOne:adminAppSecret"];
+            var envId = ConfigurationManager.AppSettings["PingOne:envID"];
+            string tokenURL = $"{authPath}/{envId}/as/token";
+
+            var basicAuth = "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"));
+
+
+            var client = new RestClient(tokenURL);
+            //client.Timeout = -1;
+            var request = new RestRequest("", Method.Post);
+            request.AddHeader("Authorization", basicAuth);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("grant_type", "client_credentials");
+            RestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content
+            if (response.IsSuccessful)
+            {
+                JsonDocument jsonResponse = JsonDocument.Parse(response.Content);
+                var accessToken = jsonResponse.RootElement.GetProperty("access_token").GetString();
+                return accessToken;
+            }
+            return "";
+        }
+        public async Task<string> Register(RegistrationViewModel model)
+        {
+            try
+            {
+                var apiPath = ConfigurationManager.AppSettings["PingOne:apiPath"];
+                var envId = ConfigurationManager.AppSettings["PingOne:envID"];
+                var popID = ConfigurationManager.AppSettings["PingOne:popID"];
+
+                var client = new RestClient($"{apiPath}/environments/{envId}/users");
+                string accessToken = await GetAccessToken();
+                //string ssha512Pass = CalculateSHA512Hash(model.password);
+                var requestModel = new RegistrationModel()
+                {
+                    email = model.email,
+                    name = new Name { given = model.firstName, family = model.lastName },
+                    population = new Population { id = popID },
+                    username = model.email,//$"{model.firstName}_{DateTime.UtcNow.ToString("ddMMyyHHmm")}",
+                    department = "User",
+                    locales = new List<string> { "Dhaka", "Bangladesh" },
+                    password = new Password { value = model.password, forceChange = false },
+                };
+
+                var request = new RestRequest("", Method.Post);
+                request.AddHeader("Authorization", $"Bearer {accessToken}");
+                request.AddHeader("content-type", "application/vnd.pingidentity.user.import+json");
+                request.AddParameter("application/vnd.pingidentity.user.import+json", JsonSerializer.Serialize(requestModel), ParameterType.RequestBody);
+                RestResponse response = client.Execute(request);
+                return response.Content.ToString();
+
             }
             catch (Exception)
             {
